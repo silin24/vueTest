13dff18cd1401886083678e350a6916cd36e4138
silin24 <silin24@foxmail.com>
2020-12-13 13:38:58 +0800

    1.txt

---
+++ demo.js
@@ -0,0 +1,15 @@
+import...from
+import from ""
+import
+im
+bas
+a
+vm.compileFunction(, params, options, = , {});
+vbase
+vbase
+vm.isContext(object);
+data() {
+    return {
+        name: data
+    };
+}
